78d8c3983eb5fc86494c4081f1885b6d60943f98
Dulina77 <dulinawickramasinghe@gmail.com>
2025-03-02 21:57:34 +0530

    Updates

--- EcoGrow/Disease_Prediction/routes.py
+++ EcoGrow/Disease_Prediction/routes.py
@@ -3,7 +3,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 import os
-from .. import disease_identification_bp 
+from . import disease_identification_bp 
 
 app = Flask(__name__)
 
--- EcoGrow/main.py
+++ EcoGrow/main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from space_identification import space_identification_bp  
+from Disease_Prediction import disease_identification_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(space_identification_bp, url_prefix='/space_identification')
+app.register_blueprint(disease_identification_bp, url_prefix = '/disease_identification')
 
 
 if __name__ == '__main__':
